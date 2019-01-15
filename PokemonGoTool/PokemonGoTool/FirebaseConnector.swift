7e3866777ee61516ead4161153b4d45572d50382
Nico Stanciu <iphone5001@gmail.com>
2019-01-15 01:33:37 +0100

    added basic firebase connection writing (reading in progress)

--- PokemonGoTool/PokemonGoTool/FirebaseConnector.swift
+++ PokemonGoTool/PokemonGoTool/FirebaseConnector.swift
@@ -2,6 +2,54 @@
 import Foundation
 import Firebase
 
+struct Quest {
+    let name: String
+    let reward: String
+    let latitude: Double
+    let longitude: Double
+    let submitter: String
+    //upvotes
+    //downvotes
+}
+
 class FirebaseConnector {
     
+    private var database: DatabaseReference!
+    
+    init() {
+        database = Database.database().reference()
+        
+//        let quest = Quest(name: "Foo", reward: "bar", latitude: 48.41235, longitude: 11.23564, submitter: "Developer")
+//        saveQuest(quest)
+        let _ = loadQuests()
+    }
+    
+    func saveQuest(_ quest: Quest) {
+        let geohash = "\(Geohash.encode(latitude: quest.latitude, longitude: quest.longitude))"
+        
+        let data = ["name"      : quest.name,
+                    "reward"    : quest.reward,
+                    "latitude"  : "\(quest.latitude)",
+                    "longitude" : "\(quest.longitude)",
+                    "submitter" : "\(quest.submitter)"]
+        
+        database.child("quests").child(geohash).childByAutoId().setValue(data)
+    }
+    
+    func loadQuests() -> [Quest] {
+        database.observe(.childAdded, with: { snapshot in
+            
+//            snapshot.children.forEach { print($0) }
+            
+            
+//            print(snapshot.key)
+//            guard let dict = snapshot.value as? [String : Any] else { return }
+            print(snapshot.value(forKey: "quests") ?? "")
+            
+            
+//            print(dict)
+//            print(snapshot.childSnapshot(forPath: "quests"))
+        })
+        return [Quest(name: "Foo", reward: "bar", latitude: 48.41235, longitude: 11.23564, submitter: "Developer")]
+    }
 }
--- PokemonGoTool/PokemonGoTool/MapViewController.swift
+++ PokemonGoTool/PokemonGoTool/MapViewController.swift
@@ -6,11 +6,13 @@ class MapViewController: UIViewController, MKMapViewDelegate, LocationManagerDel
 
     @IBOutlet private var mapView: MKMapView!
     private var locationManager = LocationManager()
-
+    private var firebaseConnector: FirebaseConnector!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         mapView.delegate = self
+        firebaseConnector = FirebaseConnector()
     }
     
     override func viewDidAppear(_ animated: Bool) {
