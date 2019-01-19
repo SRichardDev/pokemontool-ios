8a9070dadacff0fe5e46470f4982171537c6910c
Nico Stanciu <iphone5001@gmail.com>
2019-01-19 01:32:51 +0100

    better button handling with login

--- PokemonGoTool/PokemonGoTool/AccountViewController.swift
+++ PokemonGoTool/PokemonGoTool/AccountViewController.swift
@@ -6,29 +6,38 @@ class AccountViewController: UIViewController, AppModuleAccessible, FirebaseStat
     var firebaseConnector: FirebaseConnector!
     var locationManager: LocationManager!
     
+    @IBOutlet var emailLabel: UILabel!
     @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordLabel: UILabel!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
-    @IBOutlet var signOutButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        signUpButton.setTitleColor(.lightGray, for: .disabled)
         updateButtons()
     }
     
     @IBAction func loginTapped(_ sender: Any) {
-        guard let email = emailTextField.text else {return}
-        guard let password = passwordTextField.text else {return}
-        
-        firebaseConnector.signInUser(with: email, password: password) { status in
-            self.showAlert(for: status)
-            self.updateButtons()
+        if firebaseConnector.isSignedIn {
+            do {
+                try Auth.auth().signOut()
+                showAlert(for: .signedOut)
+                self.updateButtons()
+            } catch let error {
+                showAlert(for: .unknown(error: error.localizedDescription))
+            }
+        } else {
+            guard let email = emailTextField.text else {return}
+            guard let password = passwordTextField.text else {return}
+            
+            firebaseConnector.signInUser(with: email, password: password) { status in
+                self.showAlert(for: status)
+                self.updateButtons()
+            }
         }
+
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
@@ -46,20 +55,21 @@ class AccountViewController: UIViewController, AppModuleAccessible, FirebaseStat
         updateButtons()
     }
     
-    @IBAction func signOut() {
-        do {
-            try Auth.auth().signOut()
-            showAlert(for: .signedOut)
-        } catch let error {
-            showAlert(for: .unknown(error: error.localizedDescription))
-        }
-        updateButtons()
+    func updateButtons() {
+        let isSignedIn = firebaseConnector.isSignedIn
+        signUpButton.isEnabled = !isSignedIn
+        isSignedIn ? loginButton.setTitle("Sign out", for: .normal) : loginButton.setTitle("Sign in", for: .normal)
+        isSignedIn ? (signUpButton.backgroundColor = #colorLiteral(red: 0.94599998, green: 0.4300000072, blue: 0.2220000029, alpha: 1).withAlphaComponent(0.5)) : (signUpButton.backgroundColor = #colorLiteral(red: 0.94599998, green: 0.4300000072, blue: 0.2220000029, alpha: 1))
+        emailTextField.isEnabled = !isSignedIn
+        passwordLabel.isHidden = isSignedIn
+        passwordTextField.isHidden = isSignedIn
+        
+        
     }
     
-    func updateButtons() {
-        loginButton.isEnabled = !firebaseConnector.isSignedIn
-        signUpButton.isEnabled = !firebaseConnector.isSignedIn
-        signOutButton.isEnabled = firebaseConnector.isSignedIn
+    @IBAction func viewTapped(_ sender: Any) {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
     }
 }
 
