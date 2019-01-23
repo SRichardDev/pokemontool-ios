abb9e28e0d5a965e27ceb5b64150bc804627b3b6
Nico Stanciu <iphone5001@gmail.com>
2019-01-23 02:19:00 +0100

    cloud function thats sends push to registered device with change of the pokestop name

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -21,13 +21,81 @@ admin.initializeApp(functions.config().firebase);
 //     });
 // })
 
-exports.sendAdminNotification = functions.database.ref('/pokestops/{pushId}').onWrite(event => {
-         const payload = {
-             notification: {
-                title: `New Quest`,
-                body: `Foo Foooooo`,
-                sound: `default`
-             }
-         };
-    admin.messaging().sendToTopic("pokestops", payload)
+// exports.sendNotification = functions.database.ref('/pokestops/{pushId}').onWrite(event => {
+//          const payload = {
+//              notification: {
+//                 title: `New Quest`,
+//                 body: `Foo Foooooo`,
+//                 sound: `default`
+//              }
+//          };
+//     admin.messaging().sendToTopic("pokestops", payload)
+// });
+
+
+
+exports.sendPush = functions.database.ref('/pokestops/{geohash}/{uid}').onWrite((snapshot, context) => {
+    // const path = context.params;
+    // console.log(path);
+    const geohash = context.params.geohash;
+    const uid = context.params.uid;
+    console.log("geohash: " + geohash);
+    console.log("uid: " + uid);
+    console.log("snapshot: " + snapshot);
+
+    const currentKey = snapshot.after.key;
+    console.log("currentKey: " + currentKey);
+
+    const pokestop = snapshot.after.val(); //Message Data
+    console.log("pokestop name: " + pokestop.name);
+
+    const name = pokestop.name;
+    // const users = currentValue.registred_user;
+    // const token = users.pushToken;
+    // console.log(token);
+    // var userId = admin.auth().currentUser.uid;
+    // console.log("userId: " + userId);
+
+    admin.database().ref('/pokestops/' + geohash + '/registred_user').once('value', (snapshot, context) => { 
+        var notificationToken = (snapshot.val() && snapshot.val().pushToken) || 'No token';
+        console.log("notificationToken: " + notificationToken);
+        console.log("new snapshot");
+        console.log(snapshot.val());
+        console.log("new context");
+        console.log(context);
+
+        const payload = {
+            notification: {
+               title: 'New Quest',
+               body: name,
+               sound: 'default'
+            }
+        };
+    
+        admin.messaging().sendToDevice(notificationToken, payload)
+    });
+
+    
+
+    // currentValue.name
+    // currentValue.type
+    // currentValue.color
+
+//   admin.database.ref('/pokestops/${path}/registred_users').once('value', (snapshot) => { 
+//         const user = snapshot.val();
+//         console.log(user);
+//   });
+//     var subscriber = snapshot.val();
+
+//     subscriber.pushToken
+//     subscriber.filters
+
+//     if (filters.contains(currentValue.type)) {
+//       tokens.push(subscriber.token);
+
+//       // send push
+//       admin.messaging().sendToDevice(tokens, payload).then(response => {
+//         // For each message check if there was an error.
+//       });
+//     }
 });
