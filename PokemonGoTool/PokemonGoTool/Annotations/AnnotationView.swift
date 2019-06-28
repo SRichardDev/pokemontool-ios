dedf7f2fc15312968ee89f13d02b705e5b3b101d
Nico Stanciu <iphone5001@gmail.com>
2019-06-28 19:53:19 +0200

    refactored image handling for annotation

--- PokemonGoTool/PokemonGoTool/Annotations/AnnotationView.swift
+++ PokemonGoTool/PokemonGoTool/Annotations/AnnotationView.swift
@@ -130,11 +130,18 @@ class AnnotationView: CustomAnnotationView {
         if let raid = annotation.arena?.raid, !raid.isExpired {
             
             let raidboss = RaidbossManager.shared.raidboss(for: raid.raidBossId)
-            guard let topImage = ImageManager.image(named: "\(raidboss?.imageName ?? "")") ?? ImageManager.image(named: "level_\(raid.level)") else { return annotationView }
             
-            let scaleFactor: CGFloat = raid.hasHatched ? 4 : 2
+            let topImage: UIImage
+            let scaleFactor: CGFloat = raid.hasHatched ? ((raid.raidBossId != nil) ? 4 : 1.5) : 1.5
+
+            let raidBossImage = ImageManager.image(named: "\(raidboss?.imageName ?? "")")
+            let eggImage = ImageManager.image(named: "level_\(raid.level)")
+            let eggHatchedImage = ImageManager.image(named: "level_\(raid.level)_hatched")
+            let hatchedTopImage = raidBossImage ?? eggHatchedImage ?? UIImage()
+            let notHatchedTopImage = eggImage ?? UIImage()
+            topImage = raid.hasHatched ? hatchedTopImage : notHatchedTopImage
             
-            let size = CGSize(width: topImage.size.width/scaleFactor, height: topImage.size.height/scaleFactor)
+            let size = CGSize(width: topImage.size.width / scaleFactor, height: topImage.size.height / scaleFactor)
             let resizedTopImage = topImage.resize(targetSize: size)
             annotationView.image = UIImage.imageByCombiningImage(firstImage: baseImage, withImage: resizedTopImage)
             
