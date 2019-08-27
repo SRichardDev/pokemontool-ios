18717322cbe51e848e4d87e69eb0af605989a56a
Nico Stanciu <iphone5001@gmail.com>
2019-08-27 20:09:35 +0200

    cleanup

--- PokemonGoTool/PokemonGoTool/Map/MapFilter/MapFilterViewController.swift
+++ PokemonGoTool/PokemonGoTool/Map/MapFilter/MapFilterViewController.swift
@@ -13,47 +13,6 @@ class MapFilterViewController: UIViewController, StoryboardInitialViewController
         stackView.addToView(view)
         stackView.addArrangedViewController(arenaFilterViewController, to: self)
         stackView.addArrangedViewController(pokestopFilterViewController, to: self)
-        
-//        let arenaFilter = LabelSwitchRow()
-//        let arenaOnlyEXFilter = LabelSwitchRow()
-//        let arenaOnlyRaidFilter = LabelSwitchRow()
-//
-//        arenaFilter.setup("Arenas", isOn: AppSettings.showArenas) { isOn in
-//            AppSettings.showArenas = isOn
-//            arenaOnlyRaidFilter.changeVisibility(isOn)
-//            arenaOnlyEXFilter.changeVisibility(isOn)
-//        }
-//
-//        arenaOnlyEXFilter.setup("Nur EX", isOn: AppSettings.showOnlyEXArenas, isSubRow: true) { isOn in
-//            AppSettings.showOnlyEXArenas = isOn
-//        }
-//
-//        arenaOnlyRaidFilter.setup("Nur mit Raid", isOn: AppSettings.showOnlyArenasWithRaid, isSubRow: true) { isOn in
-//            AppSettings.showOnlyArenasWithRaid = isOn
-//        }
-//
-//        arenasFilterStackView.setup(mainRow: arenaFilter, subRows: [arenaOnlyRaidFilter,
-//                                                                    arenaOnlyEXFilter])
-//
-//
-//
-//        let pokestopFilter = LabelSwitchRow()
-//        let pokestopOnlyQuestFilterRow = LabelSwitchRow()
-//
-//        pokestopFilter.setup("Pokestops", isOn: AppSettings.showPokestops) { isOn in
-//            pokestopOnlyQuestFilterRow.changeVisibility(isOn)
-//            AppSettings.showPokestops = isOn
-//        }
-//
-//        pokestopOnlyQuestFilterRow.setup("Nur mit Quest", isOn: AppSettings.showOnlyPokestopsWithQuest, isSubRow: true) { isOn in
-//            AppSettings.showOnlyPokestopsWithQuest = isOn
-//        }
-//
-//        pokstopsFilterStackView.setup(mainRow: pokestopFilter, subRows: [pokestopOnlyQuestFilterRow])
-//
-//
-//        stackView.addArrangedSubview(arenasFilterStackView)
-//        stackView.addArrangedSubview(pokstopsFilterStackView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
